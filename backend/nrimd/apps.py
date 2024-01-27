70e31d1079716fae74be7f490ec90cd937b9f768
Yi He <111960297+heyigacu@users.noreply.github.com>
2024-01-27 16:58:02 +0800

    Delete backend/nrimd directory

--- backend/nrimd/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class NrimdConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'nrimd'
--- backend/nrimd/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-# from .models import CaTrajectoryPDBModel, StructurePDBModel, AaTrajectoryPDBModel, JobModel
-from .models import JobModel
-
-class JobSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = JobModel
-        fields = '__all__'
